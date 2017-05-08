46bfc3809a7cbb90f55ba4b8aa7aba4fb040ead4
cyryl91 <91.cyryl@gmail.com>
2017-05-08 18:02:58 +0200

    new repository

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function drewTree(iloscWierszy){ 
+  debugger;
+  for( var i=1 ; i <= iloscWierszy; i++){
+     var star= '';
+    for( var j=0 ; j<2*i-1; j++){
+        star += '*';       
+      }
+    console.log(star);
+  }
+}
+var wysokosc = prompt('Podaj wysokosc drzewka');
+drewTree(wysokosc);
